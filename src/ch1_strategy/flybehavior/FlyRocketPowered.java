653a465ad42496eae47e96ada23e1f02afb6592e
Ruslan <zhd.ruslan@gmail.com>
2017-07-02 12:30:13 +0300

    [add fly rocket behavior]

---
+++ src/ch1_strategy/flybehavior/FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package ch1_strategy.flybehavior;
+
+public class FlyRocketPowered implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("I'm flying with a rocket!");
+    }
+}
