b6b70ddeac0e7285b61403dd0b7c8717c3e4a872
Ruslan <zhd.ruslan@gmail.com>
2017-07-03 22:30:23 +0300

    [apd all]

--- src/ch2_observer/CurrentConditionsDisplay.java
+++ src/ch2_observer/CurrentConditionsDisplay.java
@@ -11,7 +11,7 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement {
     }
 
     @Override
-    public void update(float temp, float humidity, float pressure) {
+    public void update(float temperature, float humidity, float pressure) {
         this.temperature = temperature;
         this.humidity = humidity;
         display();
--- src/ch2_observer/Observer.java
+++ src/ch2_observer/Observer.java
@@ -1,5 +1,5 @@
 package ch2_observer;
 
 public interface Observer {
-    public void update(float temp, float humidity, float pressure);
+    public void update(float temperature, float humidity, float pressure);
 }
