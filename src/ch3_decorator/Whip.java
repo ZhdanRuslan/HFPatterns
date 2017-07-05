2cf25314aee589bfc6c7f37372b30e3ff3b111b9
Ruslan <zhd.ruslan@gmail.com>
2017-07-05 22:00:36 +0300

    [add whip]

---
+++ src/ch3_decorator/Whip.java
@@ -0,0 +1,19 @@
+package ch3_decorator;
+
+public class Whip extends CondimentDecorator{
+    Beverage beverage;
+
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+
+    @Override
+    public double cost() {
+        return .70 + beverage.cost();
+    }
+}
