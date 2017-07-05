0ab30afa43da524f86b99b2174222a2e801ee262
Ruslan <zhd.ruslan@gmail.com>
2017-07-05 21:20:25 +0300

    [beverage class]

--- src/ch3_decorator/Beverage.java
+++ src/ch3_decorator/Beverage.java
@@ -1,4 +1,12 @@
 package ch3_decorator;
 
-public class Beverage {
+public abstract class Beverage {
+
+    String description = "Unknown Beverage";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
 }
