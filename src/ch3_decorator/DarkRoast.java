a1097eb5abf059c0f5a41754c70f40c72f62b144
Ruslan <zhd.ruslan@gmail.com>
2017-07-06 20:46:18 +0300

    [add DarkRoast]

---
+++ src/ch3_decorator/DarkRoast.java
@@ -0,0 +1,13 @@
+package ch3_decorator;
+
+public class DarkRoast extends Beverage {
+
+    public DarkRoast() {
+        description = "DarkRoast";
+    }
+
+    @Override
+    public double cost() {
+        return 2.99;
+    }
+}
