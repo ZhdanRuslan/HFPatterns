ff13e77d1677b86844d63d27f7d9ac2b8748e655
Ruslan <zhd.ruslan@gmail.com>
2017-07-09 18:35:45 +0300

    [NY style cheese impl]

--- src/ch4_factory/NYStyleCheesePizza.java
+++ src/ch4_factory/NYStyleCheesePizza.java
@@ -1,4 +1,12 @@
 package ch4_factory;
 
 public class NYStyleCheesePizza extends Pizza {
+
+    public NYStyleCheesePizza() {
+        name = "HY Style Sauce and Cheese Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+
+        toppings.add("Grated Reggiano Cheese");
+    }
 }
