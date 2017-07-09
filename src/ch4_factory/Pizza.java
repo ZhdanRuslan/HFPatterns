8ac277fb93ba9f81cac76d677e24b1ae68f73d49
Ruslan <zhd.ruslan@gmail.com>
2017-07-09 16:49:22 +0300

    [add fields]

--- src/ch4_factory/Pizza.java
+++ src/ch4_factory/Pizza.java
@@ -1,6 +1,13 @@
 package ch4_factory;
 
-public class Pizza {
+import java.util.ArrayList;
+
+public abstract class Pizza {
+    String name;
+    String dough;
+    String sauce;
+    ArrayList toppings = new ArrayList();
+
     public void prepare() {
     }
 
