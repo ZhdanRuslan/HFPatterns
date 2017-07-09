332c8e80b912e1c14f9e44e18075d76f49e74799
Ruslan <zhd.ruslan@gmail.com>
2017-07-09 18:47:46 +0300

    [add ChicagoPizzaStore ]

---
+++ src/ch4_factory/ChicagoPizzaStore.java
@@ -0,0 +1,11 @@
+package ch4_factory;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+    @Override
+    protected Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else return null;
+    }
+}
