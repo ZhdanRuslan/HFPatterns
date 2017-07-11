52615a4f1a9a9264fc54d0f16af412d2d536f3e6
Ruslan <zhd.ruslan@gmail.com>
2017-07-11 21:50:25 +0300

    [upd]

--- src/ch4_factory/PizzaTestDrive.java
+++ src/ch4_factory/PizzaTestDrive.java
@@ -6,6 +6,7 @@ public class PizzaTestDrive {
     public static void main(String[] args) {
         PizzaStore nyStore = new NYPizzaStore();
 
+        
         Pizza pizza = nyStore.orderPizza("cheese");
         System.out.println("Etan ordered a " + pizza.getName() + "\n");
     }
