5260e4c7e71af5454591bd7ed990e4bbc2466374
Ruslan <zhd.ruslan@gmail.com>
2017-07-11 21:32:16 +0300

    [add VeggiePizza]

---
+++ src/ch4_factory/VeggiePizza.java
@@ -0,0 +1,18 @@
+package ch4_factory;
+
+public class   extends Pizza {
+
+    PizzaIngredientFactory ingredientFactory;
+
+    VeggiePizza(PizzaIngredientFactory ingredientFactory) {
+        this.ingredientFactory = ingredientFactory;
+    }
+
+    @Override
+    public void prepare() {
+        System.out.println("Preparing " + name);
+        dough = ingredientFactory.createDough();
+        sauce = ingredientFactory.createSauce();
+        cheese = ingredientFactory.createCheese();
+    }
+}
