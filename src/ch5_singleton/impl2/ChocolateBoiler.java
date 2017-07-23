c17e4a71961bbe8a822c49445f5a5c4e16f7bfed
Ruslan <zhd.ruslan@gmail.com>
2017-07-23 13:35:30 +0300

    [fill ChocolateBoiler]

--- src/ch5_singleton/impl2/ChocolateBoiler.java
+++ src/ch5_singleton/impl2/ChocolateBoiler.java
@@ -1,4 +1,38 @@
 package ch5_singleton.impl2;
 
 public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    public ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
 }
