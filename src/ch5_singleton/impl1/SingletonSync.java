d56bb9fa5c9544d151cc91da4640852475ff0da0
Ruslan <zhd.ruslan@gmail.com>
2017-07-23 18:44:12 +0300

    [add synchronized singleton]

---
+++ src/ch5_singleton/impl1/SingletonSync.java
@@ -0,0 +1,15 @@
+package ch5_singleton.impl1;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
