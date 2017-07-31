4feb23de34a82b34261ddc7576e9ab14310dd27e
Ruslan <zhd.ruslan@gmail.com>
2017-07-31 20:21:45 +0300

    [fill light on command class]
    implements light command class

--- src/ch6_command/LightOnCommand.java
+++ src/ch6_command/LightOnCommand.java
@@ -1,4 +1,14 @@
 package ch6_command;
 
-public class LightOnCommand {
+public class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.on();
+    }
 }
