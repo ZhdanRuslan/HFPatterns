9aff73ce4c4962247e948b76ab98729f4da70e77
Ruslan <zhd.ruslan@gmail.com>
2017-08-08 22:05:52 +0300

    [add obj command]

---
+++ src/ch6_command/SimpleRemoteControl.java
@@ -0,0 +1,16 @@
+package ch6_command;
+
+public class SimpleRemoteControl {
+    Command slot;
+
+    public SimpleRemoteControl() {
+    }
+
+    public void setSlot(Command slot) {
+        this.slot = slot;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
