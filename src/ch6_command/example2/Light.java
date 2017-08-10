8dd891920faf90ae913fe4d92869b5449e41fd4b
Ruslan <zhd.ruslan@gmail.com>
2017-08-10 21:39:13 +0300

    [add Light Class]

---
+++ src/ch6_command/example2/Light.java
@@ -0,0 +1,12 @@
+package ch6_command.example2;
+
+public class Light {
+
+    public void on() {
+        System.out.println("ON");
+    }
+
+    public void off() {
+        System.out.println("OFF");
+    }
+}
