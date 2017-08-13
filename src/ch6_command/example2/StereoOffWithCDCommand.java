0b602f75b6bf6e4e4c780a3c109bc929bb0feac9
Ruslan <zhd.ruslan@gmail.com>
2017-08-13 13:11:34 +0300

    [add stereo off command]

---
+++ src/ch6_command/example2/StereoOffWithCDCommand.java
@@ -0,0 +1,15 @@
+package ch6_command.example2;
+
+public class StereoOffWithCDCommand implements Command {
+
+    Stereo stereo;
+
+    public StereoOffWithCDCommand(Stereo stereo) {
+        this.stereo = stereo;
+    }
+
+    @Override
+    public void execute() {
+        stereo.off();
+    }
+}
