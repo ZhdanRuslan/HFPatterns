3941af323915515acdfbb7d62567479c553dd0e6
Ruslan <zhd.ruslan@gmail.com>
2017-08-13 13:13:15 +0300

    [impl RemoteLoader]

--- src/ch6_command/example2/RemoteLoader.java
+++ src/ch6_command/example2/RemoteLoader.java
@@ -1,4 +1,25 @@
 package ch6_command.example2;
 
 public class RemoteLoader {
+    public static void main(String[] args) {
+        RemoteControl remoteControl = new RemoteControl();
+
+        Light livingRoomLight = new Light("Living Room");
+        Stereo livingRoomStereo = new Stereo("Living Room");
+
+        LightOnCommand lightOnCommand = new LightOnCommand(livingRoomLight);
+        LightOffCommand lightOffCommand = new LightOffCommand(livingRoomLight);
+
+        StereoOnWithCDCommand stereoOnWithCDCommand = new StereoOnWithCDCommand(livingRoomStereo);
+        StereoOffWithCDCommand stereoOffWithCDCommand = new StereoOffWithCDCommand(livingRoomStereo);
+
+        remoteControl.setCommand(0, lightOnCommand, lightOffCommand);
+        remoteControl.setCommand(1, stereoOnWithCDCommand, stereoOffWithCDCommand);
+
+        remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(0);
+
+        remoteControl.onButtonWasPushed(1);
+        remoteControl.offButtonWasPushed(1);
+    }
 }
