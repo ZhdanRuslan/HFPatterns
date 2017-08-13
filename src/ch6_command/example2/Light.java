890f495aef6bb5db80c799d9ee18bbc925552af5
Ruslan <zhd.ruslan@gmail.com>
2017-08-13 13:06:30 +0300

    [add constructors in title]

--- src/ch6_command/example2/Light.java
+++ src/ch6_command/example2/Light.java
@@ -2,11 +2,17 @@ package ch6_command.example2;
 
 public class Light {
 
+    private String title;
+
+    public Light(String s) {
+        title = s;
+    }
+
     public void on() {
-        System.out.println("ON");
+        System.out.println("ON in " + title);
     }
 
     public void off() {
-        System.out.println("OFF");
+        System.out.println("OFF in " + title);
     }
 }
--- src/ch6_command/example2/Stereo.java
+++ src/ch6_command/example2/Stereo.java
@@ -3,21 +3,26 @@ package ch6_command.example2;
 public class Stereo {
 
     private int volume;
+    private String title;
+
+    public Stereo(String s) {
+        title = s;
+    }
 
     public void on(){
-        System.out.println("ON");
+        System.out.println("ON in " + title);
     }
 
     public void off(){
-        System.out.println("OFF");
+        System.out.println("OFF in " + title);
     }
 
     public void setCd(){
-        System.out.println("CD was chosen.");
+        System.out.println("CD was chosen. In " + title);
     }
 
     public void setVolume(int volume){
         this.volume = volume;
-        System.out.println("Volume level is setting on :" + volume);
+        System.out.println("Volume level is setting on :" + volume + " in " + title);
     }
 }
