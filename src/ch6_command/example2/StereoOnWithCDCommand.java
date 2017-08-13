a4944601c115d04180062730dc8d2fa0c0c92bd3
Ruslan <zhd.ruslan@gmail.com>
2017-08-13 13:00:45 +0300

    [add command for Stereo CD]

---
+++ src/ch6_command/example2/StereoOnWithCDCommand.java
@@ -0,0 +1,17 @@
+package ch6_command.example2;
+
+public class StereoOnWithCDCommand implements Command {
+
+    Stereo stereo;
+
+    public StereoOnWithCDCommand(Stereo stereo) {
+        this.stereo = stereo;
+    }
+
+    @Override
+    public void execute() {
+        stereo.on();
+        stereo.setCd();
+        stereo.setVolume(10);
+    }
+}
